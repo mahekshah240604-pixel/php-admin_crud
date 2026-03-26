0441678d017b40e89119a629b5cdf698e6861208
Mahek Shah <mahek.shah240604@gmail.com>
2026-03-26 13:34:31 +0530

    Add PHP CRUD project files
    This project is a simple CRUD application using PHP and MySQL.
    It includes create, read, update, and delete functionality.

---
+++ thimpart/sidebar.php
@@ -0,0 +1,171 @@
+<aside class="sidebar-left">
+      <nav class="navbar navbar-inverse">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html"><span class="fa fa-area-chart"></span> Jewllery<span class="dashboard_text">Online Shopping</span></a>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="sidebar-menu">
+              
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>admin</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-admin.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-admin.php"><i class="fa fa-angle-right"></i> view </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Cart</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-cart-details.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-cart-details.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-cart-details.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Category</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-category.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-category.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="display-category.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Customer</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-customer.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-customer.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-customer.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Feedback</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-feedback.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-feedback.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-feedback.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Offfer</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-offer.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-offer.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-offer.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Order-details</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-order-details.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-order-details.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-order-details.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Order</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-order.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-order.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-order.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Payment</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-payment.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-payment.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-payment.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Product-img</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-product-img.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-product-img.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-product-img.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Product</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-product.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-product.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-product.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Payment-mode</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-payment-mode.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-paymennt-mode.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-payment-mode.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-envelope"></i> 
+                <span>Refund</span>
+                <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="add-refund.php"><i class="fa fa-angle-right"></i> Add </a></li>
+                  <li><a href="display-refund.php"><i class="fa fa-angle-right"></i> view </a></li>
+                  <li><a href="edit-refund.php"><i class="fa fa-angle-right"></i> Edit </a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-folder"></i> <span>pages</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                  <li><a href="login.php"><i class="fa fa-angle-right"></i>Login</a></li>
+                  <li><a href="signup.php"><i class="fa fa-angle-right"></i> Register</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+      </nav>
+    </aside>
